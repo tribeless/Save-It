7b808398ee3917ca694c7c739436684487dee5a1
Brian Kyole <tribeless53@gmail.com>
2020-07-06 18:02:25 +0300

    new commmit

--- script.js
+++ script.js
@@ -9,19 +9,26 @@ loadAllEventListeners();
 function loadAllEventListeners(){
     $(".submittask").on("click", addUserTask);
 
+    
+    //document.querySelectorAll(".remove-single-item").addEventListener("click", () => alert("working"));
     //load content on page load
     document.addEventListener("DOMContentLoaded", retrieveAllUsersTasks);
 
     //removing single item from local storage and unordered list
     $(".user-task-list").click(removeSingleUserTask);
+    
 
     //remove all userstasks
     $(".clear-user-task-list").click(clearAllUsersTasks);
+
+    //remove select element
+    //
 }
 
 //counter to remove an annoying button
 let counter = 0;
 counter++;
+
 
 //just removing an annoying extra X in code(❁´◡`❁)
 function removeAnnoyingX(){
@@ -34,80 +41,110 @@ function removeAnnoyingX(){
 
     }
 }
+
+//called to scroll page to current text is
+function scrollToNewPosition(){
+    document.querySelector(".user-task-list").scrollTop = document.querySelector(".user-task-list").scrollHeight;
+}
+
 //when page loads, retrieve previous existing user tasks from local storage
     function retrieveAllUsersTasks(){
         var toDo;
-        if(localStorage.getItem("usersTasks")===null){
+       
+        if (localStorage.getItem("usersTasks") === null) {
             toDo = [];
+            
         }
         else{
             toDo = JSON.parse(localStorage.getItem("usersTasks"));
+           
         }
 
-        toDo.forEach(todo=>{
+         toDo.forEach(todo=>{
 
-            $(".user-task-list").append($("<li>").html(todo));
-            $("li").append($("<button>").html("X"));
-            $("li").children().addClass("remove-single-item");
-
-            removeAnnoyingX();
+             $(".user-task-list").append($("<li>").html(todo));
+           //  $(".user-task-list").append($("<a>").html("X"));
+             $("li").append($("<a>").html("X"));
+             
+             $("li").children().addClass("remove-single-item");
+             
+             removeAnnoyingX();
+             scrollToNewPosition();
             
-        })
+         })
+
     }
 
     //adding new task to unordered list
     function addUserTask(){
-
+        var date = new Date().toLocaleDateString();
         var $usersTaskArray = $(".simple-input").children("#userTask").val();
+        
+        
         if($usersTaskArray===""){
+
             alert("Please enter a task");
         }
         else{
-            $(".user-task-list").append($("<li>").html($usersTaskArray));
-            $("li").append($("<button>").html("X"));
+            var ads = $usersTaskArray + " " + date;
+            $(".user-task-list").append($("<li>").html(ads));
+            $("li").append($("<a>").html("X"));
+
             $("li").children().addClass("remove-single-item");
 
-            storeUserDataInLocalStorage($usersTaskArray);
-            $(".simple-input").children("#userTask").val("");
+            storeUserDataInLocalStorage(ads);
+           
         }
-        $usersTaskArray = "";
-
+         $(".simple-input").children("#userTask").val("");
+        
         
         removeAnnoyingX();
+        scrollToNewPosition();
     }
 
     //storing all user new tasks into local storage
     function storeUserDataInLocalStorage(todo){
         let toDo;
-        if (localStorage.getItem("usersTasks")===null){
+        let userDate;
+        //console.log(date)
+        if (localStorage.getItem("usersTasks") === null) {
             toDo=[];
+            
         }
         else{
             toDo = JSON.parse(localStorage.getItem("usersTasks"));
+            
         }
         toDo.push(todo);
+        
+
         localStorage.setItem("usersTasks",JSON.stringify(toDo));
+        
 
     }
 
     //removing single item from the unoerderes list
+    
     function removeSingleUserTask(e){
         var bvd = e.target;
+         
+          var singleUserTask=bvd.parentElement.innerText.replace("X", "");
         
-        var singleUserTask=bvd.parentElement.innerText.replace("X", "");
-        bvd.parentElement.remove();
-
-        removeSingleTaskFromLocalStorage(singleUserTask);
+          bvd.parentElement.remove();
+          removeSingleTaskFromLocalStorage(singleUserTask);
 
     }
 
     //removing a single item from local storage
     function removeSingleTaskFromLocalStorage(localstorageusertask){
         let toDo;
-        if (localStorage.getItem("usersTasks")===null){
+        
+        if (localStorage.getItem("usersTasks") === null) {
             toDo=[];
+            
         }else{
             toDo = JSON.parse(localStorage.getItem("usersTasks"));
+            
         }
 
         toDo.forEach((event,index)=>{
@@ -116,7 +153,11 @@ function removeAnnoyingX(){
 
             }
         });
-        localStorage.setItem("usersTasks",JSON.stringify(toDo))        ;
+
+       
+        
+        localStorage.setItem("usersTasks",JSON.stringify(toDo));
+        scrollToNewPosition();
 
     }
 
@@ -125,6 +166,7 @@ function removeAnnoyingX(){
         $("li").remove();
 
         clearAllUsersTasksFromLocalStorage();
+        scrollToNewPosition();
     }
 
     //clear all users tasks from local storage
